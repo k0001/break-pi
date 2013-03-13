84d5401f2490c4b37e78e64d1bd0bb6f270f0380
Renzo Carbonara <gnuk0001@gmail.com>
2013-03-12 22:00:53 -0300

    Screen 02 working! I copied Alex's original code this time, since mine wasn't working for a currently unknown reason

--- source/mailbox.s
+++ source/mailbox.s
@@ -1,59 +1,68 @@
+/******************************************************************************
+*	mailbox.s
+*	 by Alex Chadwick
+*
+*	A sample assembly code implementation of the screen02 operating system.
+*	See main.s for details.
+*
+*	mailbox.s contains code that interacts with the mailbox for communication
+*	with various devices.
+******************************************************************************/
+
+/* NEW
+* GetMailboxBase returns the base address of the mailbox region as a physical
+* address in register r0.
+* C++ Signature: void* GetMailboxBase()
+*/
 .globl GetMailboxBase
-GetMailboxBase:
-    ldr r0,=0x2000b880
-    mov pc,lr
+GetMailboxBase: 
+	ldr r0,=0x2000B880
+	mov pc,lr
 
-.globl MailboxWrite
-MailboxWrite:
-    tst r0,#0b1111
-    movne pc,lr
-    cmp r1,#0b1111
-    movhi pc,lr
-
-    channel .req r1
-    value .req r2
-    mov value,r0
-    push {lr}
-    bl GetMailboxBase
-    mailbox .req r0
-
-    wait1$:
-        /* Check current status */
-        ldr r3,[mailbox,#0x18]
-        tst r3,#0x80000000
-        bne wait1$
-
-    add value,channel
-    .unreq channel
-    str value,[mailbox,#0x20]
-    .unreq value
-    .unreq mailbox
-
+/* NEW
+* MailboxRead returns the current value in the mailbox addressed to a channel
+* given in the low 4 bits of r0, as the top 28 bits of r0.
+* C++ Signature: u32 MailboxRead(u8 channel)
+*/
 .globl MailboxRead
-MailboxRead:
-    cmp r0,#0b1111
-    movhi pc,lr
-    channel .req r1
-    mov channel,r0
-    push {lr}
-    bl GetMailboxBase
-    mailbox .req r0
-rightmail$:
-wait2$:
-    status .req r2
-    ldr status,[mailbox,#0x18]
-    tst status,#0x40000000
-    .unreq status
-    bne wait2$
-    mail .req r2
-    ldr mail,[mailbox,#0]
-    inchan .req r3
-    and inchan,mail,#0b1111
-    teq inchan,channel
-    .unreq inchan
-    bne rightmail$
-    .unreq mailbox
-    .unreq channel
-    and r0,mail,#0xfffffff0
-    .unreq mail
-    pop {pc}
+MailboxRead: 
+	and r3,r0,#0xf
+	mov r2,lr
+	bl GetMailboxBase
+	mov lr,r2
+	
+	rightmail$:
+		wait1$: 
+			ldr r2,[r0,#24]
+			tst r2,#0x40000000
+			bne wait1$
+			
+		ldr r1,[r0,#0]
+		and r2,r1,#0xf
+		teq r2,r3
+		bne rightmail$
+
+	and r0,r1,#0xfffffff0
+	mov pc,lr
+
+/* NEW
+* MailboxWrite writes the value given in the top 28 bits of r0 to the channel
+* given in the low 4 bits of r1.
+* C++ Signature: void MailboxWrite(u32 value, u8 channel)
+*/
+.globl MailboxWrite
+MailboxWrite: 
+	and r2,r1,#0xf
+	and r1,r0,#0xfffffff0
+	orr r1,r2
+	mov r2,lr
+	bl GetMailboxBase
+	mov lr,r2
+
+	wait2$: 
+		ldr r2,[r0,#24]
+		tst r2,#0x80000000
+		bne wait2$
+
+	str r1,[r0,#32]
+	mov pc,lr
--- source/main.s
+++ source/main.s
@@ -1,78 +1,129 @@
+/******************************************************************************
+*	main.s
+*	 by Alex Chadwick
+*
+*	A sample assembly code implementation of the screen02 operating system, that 
+*	renders pseudo random lines to the screen.
+*
+*	main.s contains the main operating system, and IVT code.
+******************************************************************************/
+
+/*
+* .globl is a directive to our assembler, that tells it to export this symbol
+* to the elf file. Convention dictates that the symbol _start is used for the 
+* entry point, so this all has the net effect of setting the entry point here.
+* Ultimately, this is useless as the elf itself is not used in the final 
+* result, and so the entry point really doesn't matter, but it aids clarity,
+* allows simulators to run the elf, and also stops us getting a linker warning
+* about having no entry point. 
+*/
 .section .init
 .globl _start
 _start:
-    b main
 
+/*
+* According to the design of the RaspberryPi, addresses 0x00 through 0x20 
+* actually have a special meaning. This is the location of the interrupt 
+* vector table. Thus, we shouldn't make the code for our operating systems in 
+* this area, as we will need it in the future. In fact the first address we are
+* really safe to use is 0x8000.
+*/
+b main
+
+/*
+* This command tells the assembler to put this code at 0x8000.
+*/
 .section .text
+
+/*
+* main is what we shall call our main operating system method. It never 
+* returns, and takes no parameters.
+* C++ Signature: void main()
+*/
 main:
-    mov sp,#0x8000
 
-    /* Ready GPIO pin 16 (led) for output */
-    mov r0,#16
-    mov r1,#1
-    bl SetGpioFunction
-    /* Blink led once */
-    bl LedBlink
+/*
+* Set the stack point to 0x8000.
+*/
+	mov sp,#0x8000
 
-    /* Initialise video frame buffer */
-    mov r0,#1024
-    mov r1,#768
-    mov r2,#16
-    bl InitialiseFrameBuffer
-    /* Check everything went OK */
-    teq r0,#0
-    bne noError$
-    bl LedOn
-    error$:
-        b error$
-    noError$:
-        fbInfoAddr .req r4
-        mov fbInfoAddr,r0
+/* 
+* Setup the screen.
+*/
+	mov r0,#1024
+	mov r1,#768
+	mov r2,#16
+	bl InitialiseFrameBuffer
 
-    render$:
-        fbAddr .req r3
-        ldr fbAddr,[fbInfoAddr,#32]
-        colour .req r0
-        y .req r1
-        mov y,#768
-        drawRow$:
-            x .req r2
-            mov x,#1024
-            drawPixel$:
-                strh colour,[fbAddr]
-                add fbAddr,#2
-                sub x,#1
-                teq x,#0
-                bne drawPixel$
-            sub y,#1
-            add colour,#1
-            teq y,#0
-            bne drawRow$
-    b render$
+/* 
+* Check for a failed frame buffer.
+*/
+	teq r0,#0
+	bne noError$
+		
+	mov r0,#16
+	mov r1,#1
+	bl SetGpioFunction
 
-    .unreq fbAddr
-    .unreq fbInfoAddr
+	mov r0,#16
+	mov r1,#0
+	bl SetGpio
 
+	error$:
+		b error$
 
-.globl LedBlink
-LedBlink:
-    bl LedOn
-    bl LedOff
+	noError$:
 
-.globl LedOn
-LedOn:
-    /* Set GPIO pin 16 LOW */
-    push {lr}
-    mov r0,#16
-    mov r1,#1
-    bl SetGpio
-    pop {pc}
+	fbInfoAddr .req r4
+	mov fbInfoAddr,r0
 
-.globl LedOff
-LedOff:
-    /* Set GPIO pin 16 HIGH */
-    push {lr}
-    mov r0,#16
-    mov r1,#1
-    bl SetGpio
-    pop {pc}
+/* NEW
+* Let our drawing method know where we are drawing to.
+*/
+	bl SetGraphicsAddress
+	
+	lastRandom .req r7
+	lastX .req r8
+	lastY .req r9
+	colour .req r10
+	x .req r5
+	y .req r6
+	mov lastRandom,#0
+	mov lastX,#0
+	mov r9,#0
+	mov r10,#0
+render$:
+	mov r0,lastRandom
+	bl Random
+	mov x,r0
+	bl Random
+	mov y,r0
+	mov lastRandom,r0
+
+	mov r0,colour
+	add colour,#1
+	lsl colour,#16
+	lsr colour,#16
+	bl SetForeColour
+		
+	mov r0,lastX
+	mov r1,lastY
+	lsr r2,x,#22
+	lsr r3,y,#22
+
+	cmp r3,#768
+	bhi render$
+	
+	mov lastX,r2
+	mov lastY,r3
+	 
+	bl DrawLine
+
+	b render$
+
+	.unreq x
+	.unreq y
+	.unreq lastRandom
+	.unreq lastX
+	.unreq lastY
+	.unreq colour
